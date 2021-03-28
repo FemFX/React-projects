ca2c3892b1854ba868bc0a30cda4b24d96051cae
Иван Гнедько <40666716+FemFX@users.noreply.github.com>
2021-03-28 09:24:42 +0300

    Create SingleColor.js

---
+++ 9-color-generator/src/SingleColor.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
+
+const SingleColor = ({ rgb, weight}) => {
+  const bcg = rgb.join(",");
+  const hex = rgbToHex(...rgb);
+  return (
+    <article className="color" style={{ backgroundColor: `rgb(${bcg})` }}>
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hex}</p>
+    </article>
+  );
+};
+
+export default SingleColor;
